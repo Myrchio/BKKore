5152c4bd2eb935a584ee479740fd8c94b3a92225
Marco Marchione <marco.marchione@galileo.galileicrema.it>
2022-12-08 16:14:09 +0100

    [0.0.1] feat: added %bkk_isFallenPlayer% placeholder

--- src/main/java/it/blastmc/Main.java
+++ src/main/java/it/blastmc/Main.java
@@ -4,38 +4,41 @@ import it.blastmc.commands.FallenPlayersCommand;
 import it.blastmc.features.FallenPlayerList;
 import it.blastmc.features.listener.BlockTimedListener;
 import it.blastmc.features.listener.NoFirstFallDamageListener;
+import it.blastmc.hook.PlaceHolderAPIHook;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     private static Main instance;
     public final FallenPlayerList fallenPlayerList = new FallenPlayerList();
+    public final String prefix = ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + ChatColor.BOLD + "BKKore" + ChatColor.DARK_GRAY + "]" + ChatColor.WHITE;
     public Main() {
         instance = this;
     }
-
-    public static void main(){
-        System.out.println();
-    }
     @Override
     public void onEnable() {
-        System.out.println("[EmeraldCore] Plugin abilitato con successo!");
+        System.out.println(prefix + "Plugin abilitato con successo!");
         getConfig().options().copyDefaults(true);
         saveConfig();
         getEvents();
         getCommands();
+        if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new PlaceHolderAPIHook().register();
+        }
     }
     @Override
     public void onDisable() {
-        System.out.println("[EmeraldCore] Plugin disabilitato con successo!");
+        System.out.println(prefix + "Plugin disabilitato con successo!");
     }
 
     private void getEvents(){
         getServer().getPluginManager().registerEvents(new BlockTimedListener(), this);
         if (this.getConfig().getBoolean("no-first-fall-damage.enabled")){
             getServer().getPluginManager().registerEvents(new NoFirstFallDamageListener(), this);
-            System.out.println("[EmeraldCore] + No first fall damage");
+            System.out.println(ChatColor.GREEN + "+ No first fall damage");
         } else {
-            System.out.println("[EmeraldCore] - No first fall damage");
+            System.out.println(ChatColor.RED + "- No first fall damage");
         }
     }
     private void getCommands(){
--- src/main/java/it/blastmc/hook/PlaceHolderAPIHook.java
+++ src/main/java/it/blastmc/hook/PlaceHolderAPIHook.java
@@ -1,22 +1,34 @@
 package it.blastmc.hook;
 
+import it.blastmc.Main;
+import it.blastmc.features.FallenPlayerList;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.OfflinePlayer;
 import org.jetbrains.annotations.NotNull;
 
 public class PlaceHolderAPIHook extends PlaceholderExpansion {
 
+    private final FallenPlayerList fallenPlayers = Main.getInstance().fallenPlayerList;
+
     @Override
     public @NotNull String getIdentifier() {
-        return null;
+        return "bkk";
     }
 
     @Override
     public @NotNull String getAuthor() {
-        return null;
+        return "Mqrchio";
     }
 
     @Override
     public @NotNull String getVersion() {
+        return "1.0.0";
+    }
+
+    public String onRequest(OfflinePlayer player, String params) {
+        if (params.equalsIgnoreCase("isFallenPlayer")){
+            return ((Boolean) fallenPlayers.search(player.getUniqueId())).toString();
+        }
         return null;
     }
 }
